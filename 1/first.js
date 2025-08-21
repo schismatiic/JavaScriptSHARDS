27b6b7010e5286d56bced3dcfbfe2e21c059c329
SCHISMATIC MOTH <leonardojofre2003@gmail.com>
2025-08-21 17:32:26 -0400

    first

---
+++ 1/first.js
@@ -0,0 +1,8 @@
+console.log("5"+3);
+console.log("5"-3);
+console.log(true+true);
+console.log([]==0);
+console.log([]===0);
+console.log(null==undefined);
+console.log(Number("42"));
+console.log(Boolean("false"));
